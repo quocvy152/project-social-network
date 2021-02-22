241138b2bfd5f6cb3995e00ef9f770a3627bd95c
QuocVy <quocvy152@gmail.com>
2021-02-22 11:01:10 +0700

    Second commit add all file in project

---
+++ models/user-coll.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const { Schema }   = mongoose;
+
+const userSchema = new Schema({
+    email   : { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+    phone   : { type: String, required: true },
+    /**
+     * Danh sách bạn của User
+     */
+    friends : [
+        { 
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ],
+    /**
+     * Danh sách những người dùng gửi kết bạn với User
+     */
+    guestFriends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ],
+    /**
+     * Danh sách người dùng User gửi kết bạn
+     */
+    friendsRequest: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ],
+    birthDay  : { type: Date, required: true },
+    createAt   : { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('user', userSchema);
+
+exports.USER_COLL = User;
